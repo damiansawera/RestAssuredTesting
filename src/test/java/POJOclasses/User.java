f02c3ca08f25199b1c71854c6ea43498a97d8ccd
Damian Sawera <damian.sawera@unit4.com>
2023-07-09 14:29:09 +0200

    Added user endpoints for CRUD operations
    added Lombok and User pojo
    
    Added first test, POJOs, fixed endpoints

---
+++ src/test/java/POJOclasses/User.java
@@ -0,0 +1,18 @@
+package POJOclasses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private int userStatus;
+}
