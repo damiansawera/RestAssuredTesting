481e92a278100488848cb8814b4052eb307154b8
Damian Sawera <damian.sawera@unit4.com>
2023-07-09 13:39:35 +0200

    added routes
    little changes

---
+++ src/test/java/Endpoints/Routes.java
@@ -0,0 +1,11 @@
+package Endpoints;
+
+public class Routes {
+    private static final String BASE_URI = "https://petstore.swagger.io/v2";
+    private static final String POST_URL = BASE_URI+"/user/{username}";
+    private static final String GET_URL = BASE_URI+"/user/{username}";
+    private static final String UPDATE_URL = BASE_URI+"/user/{username}";
+    private static final String DELETE_URL = BASE_URI+"/user/{username}";
+
+
+}
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -6,10 +6,15 @@ public class TestClass {
 
 @Test
 public void getTest(){
-   String endpoint = "https://reqres.in/api/users?page=2";
-
-        given().
-        when().get(endpoint).
+    String endpoint = "https://reqres.in/api/register/23";
+    String body =       """
+            {
+                "email": "eve.holt@reqres.in",
+                "password": "pistol"
+            }
+                        """;
+        given().body(body).
+        when().post(endpoint).
         then().log().body();
 
     }
