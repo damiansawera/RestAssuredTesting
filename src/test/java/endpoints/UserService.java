957bf6cd35049f50dc6639ebd9878d484f80e775
Damian Sawera <damian.sawera@unit4.com>
2023-07-18 15:31:01 +0200

    added properties file for routes

--- src/test/java/endpoints/UserService.java
+++ src/test/java/endpoints/UserService.java
@@ -5,62 +5,73 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import java.util.ResourceBundle;
+
 import static io.restassured.RestAssured.given;
 
 public class UserService {
-
+    static ResourceBundle getURL() {
+        ResourceBundle routes = ResourceBundle.getBundle("routes");
+        return routes;
+    }
     public static RequestSpecification requestSpec = given()
-            .baseUri(Routes.BASE_URI)
+            .baseUri(getURL().getString("base_uri"))
             .contentType(ContentType.JSON);
 
     public static Response createUser(User userPayload){
-    Response response =
+        String post_user_endpoint = getURL().getString("post_user_endpoint");
+
+        Response response =
             given().
                     spec(requestSpec).
                     body(userPayload).
             when().
-                    post(Routes.POST_USER_ENDPOINT);
+                    post(post_user_endpoint);
     return response;
     }
 
     public static Response getUser(String username){
+        String get_user_endpoint = getURL().getString("get_user_endpoint");
     Response response =
            given().
                    spec(requestSpec).
                    pathParam("username", username).
            when().
-                  get(Routes.GET_USER_ENDPOINT);
+                  get(get_user_endpoint);
     return response;
     }
 
     public static Response updateUser(String username, User payload){
+        String update_user_endpoint = getURL().getString("update_user_endpoint");
     Response response =
            given().
                   spec(requestSpec).
                   pathParam("username", username).
                   body(payload).
            when().
-                  put(Routes.UPDATE_USER_ENDPOINT);
+                  put(update_user_endpoint);
     return response;
     }
 
     public static Response deleteUser(String username){
-    Response response =
+        String delete_user_endpoint = getURL().getString("delete_user_endpoint");
+        Response response =
            given().
                    spec(requestSpec).
                    pathParam("username", username).
                    when().
-                   delete(Routes.DELETE_USER_ENDPOINT);
+                   delete(delete_user_endpoint);
     return response;
     }
     public static Response loginUser(String username, String password){
+        String login_user_endpoint = getURL().getString("login_user_endpoint");
         Response response =
                 given().
                         spec(requestSpec).
                         queryParam("username", username).
                         queryParam("password", password).
                 when().
-                        get(Routes.LOGIN_USER_ENDPOINT);
+                        get(login_user_endpoint);
         return response;
     }
 }
